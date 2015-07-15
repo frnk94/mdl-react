752e4b820b2fbbfdeceb67d0de7d5dabf34e50e5
Xhriman <seal789ie@gmail.com>
2015-07-15 16:04:49 +0800

    [Template] Toggle

---
+++ example/views/Toggle.jsx
@@ -0,0 +1,17 @@
+
+"use strict";
+
+var React = require('react');
+var MDL = require('../../index.js');
+
+module.exports = React.createClass({
+
+	render: function() {
+		return (
+			<div>
+
+			</div>
+		);
+	},
+
+});
