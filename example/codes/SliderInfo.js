0c7d42ffe68b1e4d219134df4c1f7cfd9388d65c
許恆修 <xuhengxiu@xuhengxiudeMacBook-Air.local>
2015-07-16 18:01:55 +0800

    add SliderInfo.js && mod /example/codes/Slider.jsx

---
+++ example/codes/SliderInfo.js
@@ -0,0 +1,11 @@
+/* Default */
+<MDL.Slider min = '0'	max = '100'/>
+
+/* Starting with Initial Value && step */
+<MDL.Slider min = '0'	max = '100'	defaultValue = '10'	step = '5'/>
+
+/* Disabled */
+<MDL.Slider min = '0'	max = '100'	defaultValue = '80'	disabled = 'disabled'/>
+
+/* Get Value */
+<MDL.Slider min = '0'	max = '100'	defaultValue = '40' step = '10'/>
--- example/views/Slider.jsx
+++ example/views/Slider.jsx
@@ -3,6 +3,7 @@
 
 var React = require('react');
 var MDL = require('../../components');
+var Props = require('../document/Props.jsx');
 
 module.exports = React.createClass({
 
@@ -20,32 +21,164 @@ module.exports = React.createClass({
 	},
 
 	render: function() {
+
+		var style = {
+			width : '100%',
+			maxWidth : '1200px',
+		};
+
+		var titleStyle = {
+			fontSize: '24px',
+			lineHeight: '32px',
+			paddingTop: '16px',
+			marginBottom: '12px',
+			letterSpacing: '0px',
+			fontWeight: 400,
+			color: 'rgba(0, 0, 0, 0.87)',
+		};
+
+		var exampleStyle = {
+			color: '#e0e0e0',
+			padding: '8px',
+			marginBottom: '0px',
+			fontSize: '14px',
+			lineHeight: '20px',
+			letterSpacing: '0px',
+			textTransform: 'uppercase',
+			fontWeight: 500,
+		};
+
+		var sliderStyle = {
+			borderRadius: '0 0 2px 0',
+			padding: '24px',
+			margin: '0px',
+		};
+
+		var cardStyle = {
+			width : '100%',
+			margin : '0px',
+		};
+
+		var buttonStyle = {
+			width : '100px',
+			margin : '0px 10px',
+		};
+
+		var buttonAreaStyle = {
+			width : '240px',
+			margin : '24px auto',
+			marginTop : '0px',
+		};
+
+		var prismCode = {
+			fontSize : '14px',
+			marginBottom : '0px',
+		};
+
+		var propsDetail = [
+			{
+				key : 'min',
+				type : 'string',
+				state : 'isRequired',
+				content : 'a minimum value for an <input> element',
+			},
+			{
+				key : 'max',
+				type : 'string',
+				state : 'isRequired',
+				content : 'a maximum value for an <input> element',
+			},
+			{
+				key : 'tabIndex',
+				type : 'string',
+				state : 'optional',
+				content : 'the tab order of an element',
+			},
+			{
+				key : 'step',
+				type : 'string',
+				state : 'optional',
+				content : 'the legal number intervals for an <input> element',
+			},
+			{
+				key : 'defaultValue',
+				type : 'string',
+				state : 'optional',
+				content : 'the value of an <input> element',
+			},
+		];
+
+		var eventsDetail = [
+			{
+				key : 'getValue',
+				type : 'function',
+				state : '',
+				content : 'get the value from the current sliders',
+			},
+			{
+				key : 'setValue',
+				type : 'function(inputValue)',
+				state : '',
+				content : 'Set the value in the current sliders',
+			},
+		];
+
 		return (
-			<div>
-				<MDL.Slider
-					min = '0'
-					max = '100'
-					defaultValue = '10'
-					tabIndex = '0'
-				/><br />
-				<MDL.Slider
-					min = '0'
-					max = '100'
-					defaultValue = '35'
-					tabIndex = '1'
-					disabled = 'disabled'
-				/><br />
-				<MDL.Slider
-					ref='test'
-					min = '0'
-					max = '100'
-					defaultValue = '40'
-					step = '20'
-					tabIndex = '2'
-					onChange={this.onChange}
-				/><br />
-				<button onClick={this.getValue}>get Value</button>
-				<button onClick={this.setValue}>Set value</button>
+			<div style={style}>
+				<h2 style={titleStyle}>Slider</h2>
+				<MDL.Card style={cardStyle} shadow={6}>
+					<div style={exampleStyle}>example</div>
+						<div style={sliderStyle}>
+							<MDL.Slider
+								min = '0'
+								max = '100'
+							/><br />
+							<MDL.Slider
+								min = '0'
+								max = '100'
+								defaultValue = '10'
+								step = '5'
+							/><br />
+							<MDL.Slider
+								min = '0'
+								max = '100'
+								defaultValue = '80'
+								disabled = 'disabled'
+							/><br />
+							<MDL.Slider
+								ref='test'
+								min = '0'
+								max = '100'
+								defaultValue = '40'
+								step = '10'
+								onChange={this.onChange}
+							/><br />
+							<div style={buttonAreaStyle}>
+								<MDL.Button 
+								type="RaisedButton"
+								text="Get Value"
+								style={buttonStyle}
+								isAccent={true}
+								onClick={this.getValue}
+								/>
+								<MDL.Button 
+									type="RaisedButton"
+									text="set Value"
+									style={buttonStyle}
+									isRipple={true}
+									isPrimary={true}
+									onClick={this.setValue} 
+								/>
+						</div>
+					</div>
+					<MDL.PrismCode
+						src='../codes/SliderInfo.js'
+						lang='jsx'
+						style={prismCode}
+					/>
+				</MDL.Card>
+				<Props detail={propsDetail} title="Props" />
+				<Props detail={eventsDetail} title="Methods" />
 			</div>
 		);
 	},
