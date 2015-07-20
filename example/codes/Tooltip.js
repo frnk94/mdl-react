329f838dc15c6c872a7e507e58a8548a499085db
EmilyWen <emily1992916@gmail.com>
2015-07-20 12:32:04 +0800

    修改tooltip.js

--- example/codes/Tooltip.js
+++ example/codes/Tooltip.js
@@ -1,10 +1,10 @@
-<MDL.Tooltip 
+<Tooltip 
 					element = {<i className="material-icons">add</i>}
 					text = 'Follow'
 					large = {true}
 					style = {tooltipStyle}
 />
-<MDL.Tooltip 
+<Tooltip 
 					element = 'Please move your mouse on it.'
 					text = 'this is a bird.'
 					url = 'https://dl.dropboxusercontent.com/u/107164257/CSS3Images/CSS3/bird.png'
