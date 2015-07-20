829a45cd16fd547ab50ebc7b462f942d443be274
EmilyWen <emily1992916@gmail.com>
2015-07-20 19:37:01 +0800

    fix tab width

--- example/codes/Loading.jsx
+++ example/codes/Loading.jsx
@@ -1,8 +1,8 @@
 <Loading
-		ref='test'
-		type="ProgressBar"
-		indeterminate={false}
-		percentage={this.state.percentage}
-		buffer={87}
+	ref='test'
+	type="ProgressBar"
+	indeterminate={false}
+	percentage={this.state.percentage}
+	buffer={87}
 />					
 <Loading style={loadingStyle} />
