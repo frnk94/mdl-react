654c8d2ac32f0e646ef034dae7ac8ed0dbdbbab9
EmilyWen <emily1992916@gmail.com>
2015-07-20 18:59:41 +0800

    修改tooltip props

--- example/codes/Tooltip.jsx
+++ example/codes/Tooltip.jsx
@@ -5,7 +5,7 @@
 			<i className="material-icons">add</i>
 </Tooltip>
 <Tooltip
-		text = 'this is a bird.'
+		text = 'Here is a tooltip text.'
 		url = 'https://dl.dropboxusercontent.com/u/107164257/CSS3Images/CSS3/bird.png'
 		large = {false}
 		width = {30}
--- example/views/Tooltip.jsx
+++ example/views/Tooltip.jsx
@@ -10,8 +10,8 @@ module.exports = React.createClass({
 	render: function() {
 		var propsDetail = [
 			{
-				key : 'element',
-				type : 'node',
+				key : 'children',
+				type : 'element',
 				state : 'required',
 				content : 'The picture or text that trigger the tooltip.',
 			},
@@ -44,6 +44,12 @@ module.exports = React.createClass({
 				type : 'number',
 				state : 'optional',
 				content : 'The height of the picture in tooltip.',
+			},
+			{
+				key : 'style',
+				type : 'object',
+				state : 'optional',
+				content : 'CSS style setting.',
 			},
 		];
 
@@ -80,7 +86,7 @@ module.exports = React.createClass({
 							style = {tooltipStyle}
 						>
 							<i className="material-icons">add</i>
-						</MDL.Tooltip>
+						</MDL.Tooltip> <br />
 						<MDL.Tooltip
 							text = 'this is a bird.'
 							url = 'https://dl.dropboxusercontent.com/u/107164257/CSS3Images/CSS3/bird.png'
