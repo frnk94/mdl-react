27365161991b529c8e1fdbdb07982c16d2d9995c
xpsteven <xp@fandora.co>
2015-07-22 23:00:22 +0800

    some fix

--- test/card.js
+++ test/card.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'Card Test' : function (client) {
-	client
+		client
 		.url(client.launch_url + '/#/card')
 		.waitForElementVisible('body', 1000)
 		.assert.elementPresent(".mdl-card")
--- test/grid.js
+++ test/grid.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'Grid Test' : function (client) {
-	client
+		client
 		.url(client.launch_url + '/#/grid')
 		.waitForElementVisible('body', 1000)
 		.assert.elementPresent(".mdl-grid")
