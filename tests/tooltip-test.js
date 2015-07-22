37e459fdb5be9931b42bca8cd1142bcc6111ddc7
EmilyWen <emily1992916@gmail.com>
2015-07-22 15:00:07 +0800

    新增tooltip測試

---
+++ tests/tooltip-test.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'Demo test Tooltip' : function (client) {
+    client
+      .url('http://fandora.github.io/mdl-react/')
+      .waitForElementVisible('body', 1000)
+      .click('div.mdl-layout__drawer-button')
+      .pause(500)
+      .click('[href="#tooltip"]')
+      .pause(500)
+      .moveToElement('#mdl-tooltip-1.material-icons',12,12)
+      .pause(500)
+      .moveToElement('#mdl-tooltip-2',106,9)
+      // .click('.mdl-button--accent')
+      // .click('.mdl-button--accent')
+      // .click('.mdl-button--accent')
+      // .assert.elementPresent('tbody tr:nth-child(7)')
+      // .click('thead .mdl-checkbox')
+      // .pause(500)
+      // .click('.mdl-button--primary');
+      
+  }
+};
