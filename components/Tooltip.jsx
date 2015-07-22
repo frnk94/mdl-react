b41dbaa8956cfd870b656e6cf022b8455b04dd2d
EmilyWen <emily1992916@gmail.com>
2015-07-22 11:42:55 +0800

    小修改Tooltip

--- components/Tooltip.jsx
+++ components/Tooltip.jsx
@@ -10,11 +10,10 @@ var _ = require('lodash');
 		http://www.getmdl.io/components/index.html#tooltips-section
 	Props
 		large,		是否要選擇 mdl-tool--large
-		url,			圖片來源
-		text,			tooltips要顯示的文字
-		width,		圖片寬度
-		height,		圖片高度
-	Methods
+		text,		tooltips要顯示的文字
+		style,
+		children,
+
 */
 
 var token = 1;
@@ -24,18 +23,13 @@ module.exports = React.createClass({
 	propTypes: {
 		children: React.PropTypes.element.isRequired,
 		large: React.PropTypes.bool,
-		url: React.PropTypes.string,
 		text: React.PropTypes.string,
-		width: React.PropTypes.number,
-		height: React.PropTypes.number,
 		style: React.PropTypes.object,
 	},
 
 	getDefaultProps: function() {
 		return {
 			text: 'Tooltip text',
-			width: 10,
-			height: 20,
 		};
 	},
 
--- example/codes/Tooltip.jsx
+++ example/codes/Tooltip.jsx
@@ -4,12 +4,9 @@
 >
 		<i className="material-icons">add</i>
 </Tooltip>
-<Tooltip
-	text = 'Here is a tooltip text.'
-	url = 'https://dl.dropboxusercontent.com/u/107164257/CSS3Images/CSS3/bird.png'
+<MDL.Tooltip
+	text = 'this is a bird.'
 	large = {false}
-	width = {30}
-	height = {30}
 >
 		<span>Please move your mouse on it.</span>
 <Tooltip>
