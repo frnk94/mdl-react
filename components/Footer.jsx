af41ad650d71a0b60c9ab4f27ed4dae3f55f3551
Heng-xiu <r23456999@gmail.com>
2015-07-22 12:22:29 +0800

    減少code

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,6 @@
 var React = require('react');
 
 module.exports = React.createClass({
-
 	propTypes: {
 		leftItems : React.PropTypes.shape({
 			type : React.PropTypes.oneOf(['list', 'button']).isRequired,
@@ -23,8 +22,8 @@ module.exports = React.createClass({
 			]),
 		}),
 		isMini: React.PropTypes.bool,
+		miniOrMega: React.PropTypes.string,
 	},
-
 
 	getInitialState: function() {
 		return {
@@ -32,33 +31,21 @@ module.exports = React.createClass({
 		};
 	},
 
-	_checkHorizontal: function(inputItem) {
-		if(inputItem === this.props.leftItems)
-			return 'left';
-		if(inputItem === this.props.rightItems)
-			return 'right';
-	},
-
-	_generateLogo: function(inputItem) {
-		if(inputItem.logo) {
-			// console.log('_generateLogo');
-			return React.cloneElement(inputItem.logo, {
-				className: 'mdl-logo',
-			});
-		}
-	},
-
-	_generateList: function(inputItem, miniOrmega) {
+	_generateList: function(inputItem) {
 		if(inputItem.type == 'list') {
 			// console.log('_generateList');
 			var list = inputItem.items.map(function(item, index) {
-				return <li key={index}>{item}</li>;
+				return <li 
+							key={index}
+						>
+							{item}
+						</li>;
 			});
 			return (
 				<ul
 					className={
 						"mdl-" +
-						miniOrmega +
+						this.state.miniOrMega +
 						"-footer--link-list"
 					}
 				>
@@ -68,56 +55,28 @@ module.exports = React.createClass({
 		}
 	},
 
-	_generateBtn: function(inputItem, miniOrmega) {
-		if(inputItem.type == 'button') {
-			console.log('_generateBtn');
-			var newClassName = 'mdl-'+miniOrmega+'-footer--social-btn';
-			return inputItem.items.map(function(item, index) {
-				return React.cloneElement(item, {
-					className: newClassName,
-				});
+	_generateHorizontal: function(inputItem, side) {
+		// console.log('Generate' + side + 'item');
+		var logo = null;
+		if(inputItem.logo) {
+			logo = React.cloneElement(inputItem.logo, {
+				className: 'mdl-logo',
 			});
 		}
+		return (
+			<div className={"mdl-"+this.state.miniOrMega+"-footer--"+side+"-section"}>
+				{logo}
+				{this._generateList(inputItem)}
+			</div>
+		);
 	},
-
-	_generateHorizontal: function(propsItem, leftOrRight) {
-		if(propsItem == this.props.leftItems) {
-			console.log('Generate leftItem');
-			var miniOrmega = this.props.isMini? 'mini': 'mega';
-			var leftOrRight = this._checkHorizontal(this.props.leftItems);
-			var left = (
-				<div className={"mdl-"+miniOrmega+"-footer--"+leftOrRight+"-section"}>
-					{this._generateLogo(propsItem)}
-					{this._generateList(propsItem, miniOrmega)}
-					{this._generateBtn(propsItem, miniOrmega)}
-				</div>
-			);
-			return {left}
-		}
-		if(propsItem == this.props.rightItems) {
-			console.log('Generate rightItem');
-			var miniOrmega = this.props.isMini? 'mini': 'mega';
-			var leftOrRight = this._checkHorizontal(this.props.rightItems);
-			var right = (
-				<div className={"mdl-"+miniOrmega+"-footer--"+leftOrRight+"-section"}>
-					{this._generateLogo(propsItem)}
-					{this._generateList(propsItem, miniOrmega)}
-					{this._generateBtn(propsItem, miniOrmega)}
-				</div>
-			);
-			return {right}
-		}
-	},
-
 
 	render: function() {
-		var miniOrmega = this.props.isMini? 'mini': 'mega';
 		return (
-			<footer className={"mdl-" + miniOrmega + "-footer"}>
+			<footer className={"mdl-" + this.state.miniOrMega + "-footer"}>
 				{this._generateHorizontal(this.props.leftItems, 'left')}
 				{this._generateHorizontal(this.props.rightItems, 'right')}
 			</footer>
 		);
 	}
-
 });
