0852168367fec4f7d92f18c8828820c310f8b123
EmilyWen <emily1992916@gmail.com>
2015-07-23 11:27:17 +0800

    增加isVisible

--- tests/tooltip-test.js
+++ tests/tooltip-test.js
@@ -1,15 +1,16 @@
 module.exports = {
-  'Demo test Tooltip' : function (client) {
-    client
-      .url('http://fandora.github.io/mdl-react/')
-      .waitForElementVisible('body', 1000)
-      .click('div.mdl-layout__drawer-button')
-      .pause(1000)
-      .click('[href="#tooltip"]')
-      .pause(1000)
-      .moveToElement('#mdl-tooltip-1.material-icons',12,12)
-      .pause(1000)
-      .moveToElement('#mdl-tooltip-2',106,9)
-      .end();
-  }
+	'Demo test Tooltip' : function (client) {
+		client
+			.url('http://fandora.github.io/mdl-react/')
+			.waitForElementVisible('body', 1000)
+			.click('div.mdl-layout__drawer-button')
+			.pause(1000)
+			.click('[href="#tooltip"]')
+			.pause(1000)
+			.moveToElement('#mdl-tooltip-1.material-icons',12,12)
+			.isVisible('div.mdl-tooltip.mdl-tooltip--large')
+			.moveToElement('#mdl-tooltip-2',106,9)
+			.isVisible('div.mdl-tooltip')
+			.end();
+	}
 };
