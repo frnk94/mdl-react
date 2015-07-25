77cdd8fad9b946b9707b0a8a382aa04b4ef7b9bc
xpsteven <xp@fandora.co>
2015-07-25 11:35:50 +0800

    enable complex table test

--- test_e2e/table.js
+++ test_e2e/table.js
@@ -7,13 +7,13 @@ module.exports = {
 		.click('.mdl-button--accent')
 		.pause(1000)
 		.assert.elementPresent('tbody tr:nth-child(5)')
-		// // 暫時關掉這段測試 docker 都跑不動
-		// .click('tbody .mdl-checkbox:nth-child(1)')
-		// .pause(1000)
-		// .click('.mdl-button--primary')
-		// .pause(1000)
-		// .assert.visible("div.showValueArea")
-		// .assert.containsText("div.showValueArea", "Au bar")
+		// 暫時關掉這段測試 docker 都跑不動
+		.click('tbody .mdl-checkbox:nth-child(1)')
+		.pause(1000)
+		.click('.mdl-button--primary')
+		.pause(1000)
+		.assert.visible("div.showValueArea")
+		.assert.containsText("div.showValueArea", "Au bar")
 		.end();
 	}
 };
